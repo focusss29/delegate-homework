447bcd31219774d0db3040dfccf8fb96452fea3e
Ilya Pogozhev <ilya_pogozhev29@mail.ru>
2023-06-12 00:18:01 +0600

    Initial Commit

---
+++ delegate/main.swift
@@ -0,0 +1,21 @@
+protocol AlarmDelegate: AnyObject {
+    func alarmDidRing()
+}
+class AlarmClock {
+    weak var delegate: AlarmDelegate?
+    func ringAlarm() {
+        print("Будильник сработал!!!")
+        delegate?.alarmDidRing()
+    }
+}
+class ViewController: AlarmDelegate {
+    func alarmDidRing() {
+        print("Полученое событие: будильник сработал! ")
+    }
+}
+var alarm = AlarmClock()
+var vc = ViewController()
+
+alarm.delegate = vc
+vc.alarmDidRing()
+
